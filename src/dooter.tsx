bca9f907ddc2fcb35bc80e2cb3e9225cde17cd27
altunenes <enesaltun2@gmail.com>
2024-05-07 19:04:54 +0300

    footer

---
+++ src/dooter.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="app-footer">
+      <a href="https://github.com/altunenes" target="_blank" rel="noopener noreferrer">Github</a> | 
+      <a href="https://github.com/altunenes/butter2d" target="_blank" rel="noopener noreferrer">Repo</a> | 
+      <a href="https://doi.org/10.5281/zenodo.11005158" target="_blank" rel="noopener noreferrer">Citation</a>
+      <p>License: MIT</p>
+    </footer>
+  );
+};
+
+export default Footer;
